114b5932afdd4cc99e202b4f602eb0b7db030c99
ZsomborBenedek <zsomborbeni@gmail.com>
2024-02-19 11:00:38 +0100

    fixed virtual envs for Debian

--- setuplucky.sh
+++ setuplucky.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Create a virtual environment
+python3 -m venv .lucky_elk
+
+# Activate the virtual environment
+source .lucky_elk/bin/activate
+
+# Install required Python packages
+pip install elasticsearch requests
+
 read -p "Enter Elastic IP: " elastic_ip
 read -p "Enter token: " token
 
@@ -11,4 +20,7 @@ git clone https://github.com/JPCERTCC/Lucky-Visitor-Scam-IoC.git
 python3 luckybulkuploader.py -f ./Lucky-Visitor-Scam-IoC -o lucky.ndjson -es "https://$elastic_ip:9200" -ei logs-ti_luckyvisitor -ak "$token"
 
 # Add a cronjob for running luckyingester.py
-(crontab -l ; echo "0 1 * * * /usr/bin/python3 $path/luckyingester.py -err $path/luckyvisitor_error.log -es 'https://$elastic_ip:9200' -ei logs-ti_luckyvisitor -ak '$token'") | crontab -
+(crontab -l ; echo "0 1 * * * $path/.lucky_elk/bin/python3 $path/luckyingester.py -err $path/luckyvisitor_error.log -es 'https://$elastic_ip:9200' -ei logs-ti_luckyvisitor -ak '$token'") | crontab -
+
+# Deactivate the virtual environment
+deactivate
